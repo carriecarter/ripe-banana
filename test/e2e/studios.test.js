22527142cf44c55b39a93ce7ee6f698aedba1230
carriecarter <carrie@carrie-carter.com>
2018-07-24 00:17:17 -0700

    bedtime

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -3,7 +3,7 @@ const request = require('./request');
 const { dropCollection } = require('./db');
 const { checkOk } = request;
 
-describe.only('Studios API', () => {
+describe('Studios API', () => {
 
     beforeEach(() => dropCollection('studios'));
 
