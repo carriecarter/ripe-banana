ea8b81c47756a6af16ca31824ac0b5c413dfe054
carriecarter <carrie@carrie-carter.com>
2018-07-26 17:46:14 -0700

    routes auth done working on unit test

--- test/unit/reviewer.test.js
+++ test/unit/reviewer.test.js
@@ -1,6 +1,8 @@
 const { assert } = require('chai');
-// const { getErrors } = require('./helpers');
+const { getErrors } = require('./helpers');
 const Reviewer = require('../../lib/models/reviewer');
+
+// TO DO: Add signup and signin according to Lab.md
 
 describe('Reviewer model', () => {
 
@@ -18,11 +20,10 @@ describe('Reviewer model', () => {
         assert.isUndefined(reviewer.password, 'password should not be set');
 
         reviewer.generateHash(data.password);
+
         assert.isDefined(reviewer.hash, 'hash is defined');
         assert.notEqual(reviewer.hash, data.password, 'hash not same as password');
-
         assert.isUndefined(reviewer.validateSync());
-
         assert.isTrue(reviewer.comparePassword(data.password), 'compare good password');
         assert.isFalse(reviewer.comparePassword('bad password'), 'compare bad password');
 
@@ -31,5 +32,15 @@ describe('Reviewer model', () => {
         assert.deepEqual(json, data);
         assert.isUndefined(reviewer.validateSync());
     }); 
+
+    it('validates hash', () => {
+        const reviewer = new Reviewer({});
+        const errors = getErrors(reviewer.validateSync(), 4);
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.company.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
+        
+    });
     
 });
